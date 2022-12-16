4038760730ae1230a6dc4220f423fdb349d80832
Yamen Alashkar <yamenys.alashkar@gmail.com>
2022-12-16 01:16:28 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import Weather from './Weather';
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <Weather defaultCity="Rio de Janeiro"/>
+        <footer>
+          This project was code by Renata Monteiro and is{" "}
+          <a
+            href="https://github.com/renataam/react-weather-app-renata"
+            target="blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
+}
+
